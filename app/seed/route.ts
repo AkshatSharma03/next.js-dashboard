fbe9646adf4f0593ce9b118816f39090963637e8
Akshat sharma <akshat1.sunil@gmail.com>
2025-04-14 10:38:51 -0400

    added bcryptjs instead of bcryptjs, added postInstall

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,7 +1,8 @@
 // app/seed/route.ts
 'use server';
 
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
+
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
